ee6a1167fc14138e7056d4ec9ded24576330e8d2
isaacweigner51-arch <isaacweigner51@gmail.com>
2026-08-21 13:01:06 -0700

    Adjust dialog position in menu.gd

--- menu.gd
+++ menu.gd
@@ -2901,6 +2901,7 @@ func show_tutorial_class_picker(tutorial_lesson: int) -> void:
     dialog.size = Vector2(1000, 480)
     dialog.set_anchors_preset(Control.PRESET_CENTER)
     dialog.position = -dialog.size / 2.0
+    + Vector2(0,25)
     dialog.z_index = 901
     dialog.add_theme_stylebox_override("panel", style(GOLD_COLOR, 20))
     scrim.add_child(dialog)
